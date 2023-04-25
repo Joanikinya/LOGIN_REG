a78178416d72847c9146334ce793a64b4a566630
FIRST_NAME LAST_NAME <Ikinyaj@yahoo.com>
2023-04-24 17:39:15 -0700

    first commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,65 @@
+from flask_app import app, render_template, redirect, request, session, bcrypt
+from flask_app.models.user import User
+from flask_app import flash
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['post'])
+def register():
+    
+    if not User.validate_user(request.form):
+        return redirect('/')
+    
+    
+    hashed_pw = bcrypt.generate_password_hash(request.form['password'])
+    print(bcrypt.check_password_hash(hashed_pw, 'password'))
+    
+    temp_user = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': hashed_pw
+    }
+    
+    user = User.save(temp_user)
+    
+    session['user_id'] = user
+    session['first_name']=request.form['first_name']
+    session['last_name']=request.form['last_name']
+    
+    
+    return redirect('/dashboard')
+
+@app.route('/login', methods=['post'])
+def login():
+    user = User.find_by_email(request.form['email'])
+    
+    if not user:
+        flash("invalid credentials")
+        return redirect('/')
+    
+    password_valid = bcrypt.check_password_hash(user.password, request.form['password'])
+    
+    if not password_valid:
+        flash("invalid credentials")
+        return redirect('/')
+    
+    session['user_id'] = user.id
+    session['first_name'] = user.first_name
+
+    
+    return redirect('/dashboard')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
